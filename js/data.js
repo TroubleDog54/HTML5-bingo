0627276699f744157c3762dca831fc580c7c0869
TroubleDog54 <115918107+TroubleDog54@users.noreply.github.com>
2024-03-27 11:48:42 +1100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,43 +1,43 @@
-var headerText = "Spiro";
+var headerText = "Spiro Bingo";
 
 var footerText = "<a href='https://github.com/michaellevesque/HTML5-bingo' target='_blank'>Code available on github</a>";
 
-var winText = "Winner";
+var winText = "BINGO";
 
 var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "Only Atheists can get AIDS."},
-        {"square": "There would be less rape if all women were homemakers."},
-        {"square": "You can't get pregnant from Christmas rape."},
-        {"square": "Widespread contraception use leads to increased incidence of rape."},
-        {"square": "If you read 50 Shades of Grey, your going to hell."},
-        {"square": "Dads who conceive children through rape should have full visitation rights."},
-        {"square": "Homosexuality is a choice."},
-        {"square": "Everyone would be tempted to be gay if we didn't shame homosexuality."},
-        {"square": "There was not rape before feminism."},
-        {"square": "Marriage cures AIDS."},
-        {"square": "Church Pedophilia scandals result from tolerance of homosexuals."},
-        {"square": "Girls who watch Girls will never be happy wives."},
-        {"square": "Women subconsciously want to be raped; they send chemical signals that trigger men."},
-        {"square": "If we teach kids to put condoms on bananas, we might as well teach them how to use stripper poles."},
-        {"square": "If a girl gets drunk at a party, well, you know, I'm just sayin'."},
-        {"square": "This just shows that FemiNazi agenda is false."},
-        {"square": "Want to stop date rape? Stop Dating."},
-        {"square": "You can't take back the night; you never had it in the first place."},
-        {"square": "Middle school health class turn girls into prostitutes."},
-        {"square": "The solution to rape in the military is to kick out the women."},
-        {"square": "You want to know that your fellow soldier's got you back, not that he's staring at it."},
-        {"square": "Feminism's secret goal is to see all Christian boys aborted."},
-        {"square": "Well she has had many prior sexual partners you know."},
-        {"square": "If we're going to teach kids safe sex, we might as well just inject them with AIDS."},            
-        {"square": "If we stopped all imports the US economy would be stronger"},    	
-        {"square": "Immigration is the problem, cheap labor corrupts."},    	
-        {"square": "China has spies in all US companies, stealing our ideas."}, 
-        {"square": "If your CEO is not republican he is a net drain on the economy."},    	
-        {"square": "Farm subsides are beneficial because they help the economy."},    	
-        {"square": "If Marijuana was legal all our kids would use it."} 	
+        {"square": "Complains about nobody being able to read"},
+        {"square": "Says something uncultured"},
+        {"square": ""Get out!""},
+        {"square": "Calls Edward "Foxxy""},
+        {"square": "Insults a student"},
+        {"square": "Goes off-topic for 10+ minutes"},
+        {"square": "Says "recalcitrant""},
+        {"square": "Says something racist"},
+        {"square": "Mentions Judge Judy"},
+        {"square": "Any five squares in five minutes"},
+        {"square": "Talks about Greece / Greek"},
+        {"square": ""Don't trust the government""},
+        {"square": "Talks about the phone policy"},
+        {"square": ""Your grade just went down to an E-""},
+        {"square": "Complains he's an old fat man"},
+        {"square": "Mentions elder abuse"},
+        {"square": "Yells at anyone"},
+        {"square": "Plays music randomly"},
+        {"square": "Calls Mr Dunn"},
+        {"square": "Mispronounces something"},
+        {"square": ""Thank you""},
+        {"square": "Mentions the nursing home"},
+        {"square": "Asks someone to spell a word"},
+        {"square": "Random accusation (e.g. You molester!)"},            
+        {"square": "Complains about word definitions"},    	
+        {"square": ""I don't care, really!""},    	
+        {"square": ""What……?""}, 
+        {"square": ""Have you even done any work?""},    	
+        {"square": "PLACEHOLDER"},    	
+        {"square": "PLACEHOLDER"} 	
     ]
 };
